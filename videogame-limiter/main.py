2f08f2c44ac7f91ba2e1794c18d80e5d75606f49
WarSame <graemecliffe@gmail.com>
2021-11-08 21:24:03 -0500

    Fix import

--- videogame-limiter/main.py
+++ videogame-limiter/main.py
@@ -1,7 +1,10 @@
 from kivy.app import App
 from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+from kivy.clock import Clock
 from registry import read_app_id
 import logging
+import threading
 
 
 logging.basicConfig(
@@ -16,8 +19,18 @@ logging.basicConfig(
 
 class VideogameLimiter(App):
     def build(self):
-        read_app_id()
-        return Button(text='Hello World')
+        self.layout = BoxLayout(orientation = 'vertical')
+        self.button = self.init_button()
+        self.layout.add_widget(self.button)
+        Clock.schedule_interval(self.listen_button, 5)
+        return self.layout
+
+    def init_button(self):
+        return Button(text=f'0')
+
+    def listen_button(self, *args):
+        for app_id in read_app_id():
+            self.button.text = f"{app_id}"
 
 if __name__ == "__main__":
     VideogameLimiter().run()
--- videogame-limiter/registry.py
+++ videogame-limiter/registry.py
@@ -1,10 +1,16 @@
 from winreg import *
 import logging
+import time
 
+HKEY_PATH = "SOFTWARE\Valve\Steam"
+HKEY_NAME = "RunningAppID"
 
 def read_app_id():
-    key_name = "SOFTWARE\Valve\Steam"
+    key_name = HKEY_PATH
     reg = ConnectRegistry(None, HKEY_CURRENT_USER)
     with OpenKey(reg, key_name) as key:
-        val = QueryValueEx(key, "RunningAppID")[0]
-        logging.info(f"RunningAppId: ${val}")
+        while True:
+            val = QueryValueEx(key, HKEY_NAME)[0]
+            logging.info(f"RunningAppId: {val}")
+            yield val
+            time.sleep(5)
