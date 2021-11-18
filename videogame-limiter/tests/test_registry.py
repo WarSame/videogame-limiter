e4fbec3fe2d7e66761041a224c73faee2b9604e2
WarSame <graemecliffe@gmail.com>
2021-11-17 22:07:30 -0500

    Fully working test

--- videogame-limiter/tests/test_registry.py
+++ videogame-limiter/tests/test_registry.py
@@ -5,11 +5,12 @@ from steam.client import SteamClient
 
 class TestRegistry(unittest.TestCase):
     def test_get_game_name(self):
+        dst_id = 322330
         client = SteamClient()
         client.connect()
         client.anonymous_login()
-        info = client.get_product_info(apps=[322330], timeout=10)
-        print(info)
+        info = client.get_product_info(apps=[dst_id], timeout=10)
+        assert(info["apps"][dst_id]["common"]["name"] == "Don't Starve Together")
 
 
 if __name__ == "__main__":
